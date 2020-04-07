c73870a93da7dbf54186e6d061d6822739a0d398
Zenahr Barzani <zenmatica@gmail.com>
2020-04-07 21:36:41 +0200

    added comments

--- Hello World/Main.cpp
+++ Hello World/Main.cpp
@@ -1,7 +1,15 @@
+// Everything that begins with a hash(#) is a preprocessor statement.
+// includes usually are header files.
 #include <iostream>
 
+// main() is the entry point.
+// Main functions implicitly return an int, so you don't need to return anything explicitly in the main() function.
 int main()
 {
+	// << ---> overloaded operator. ---> just a function shorthand.
+	// Without "<<" Hello World would look like this:
+	// std::cout.print("Hello, World!").print(std::endl);
+	// << pushes parameters into cout
 	std::cout << "Hello, World!" << std::endl;
 	std::cin.get();
 }
